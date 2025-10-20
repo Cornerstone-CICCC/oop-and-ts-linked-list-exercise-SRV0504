0c3d49c973c128055bba00f6538fa31aa0ece2ad
Samuel Rios <srios050402@gmail.com>
2025-10-19 18:32:46 -0700

    complete

--- exercises/ex01.js
+++ exercises/ex01.js
@@ -1,13 +1,27 @@
 // Create a function called findMiddle that returns the middle node of a singly linked list
 // Tip: Create two runners - slow and fast
-// (The slow runner moves one node at a time, the fast runner moves two nodes at a time. When the fast runner is at the very last node, then the slow runner will eventually be in the middle of the list. You can then return its value)
+// (The slow runner moves one node at a time, the fast runner moves two nodes at a time. 
+// When the fast runner is at the very last node, then the slow runner will eventually 
+// be in the middle of the list. You can then return its value)
 
 const SLL = require('../lib/SLL');
 
 function findMiddle(list) {
-  // your code here
+  let slow = list.head;
+  let fast = list.head;
+
+  // Move fast two steps and slow one step until fast reaches the end
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  // When fast reaches the end, slow will be at the middle
+  return slow ? slow.data : null;
 }
 
+
+// Test
 const list = new SLL();
 list.insertAtBack(1);
 list.insertAtBack(2);
--- exercises/ex02.js
+++ exercises/ex02.js
@@ -1,12 +1,51 @@
-// Create a function called deleteAllNodesWithValue that removes all nodes with a specific value from the DLL
-// Make sure to update the size of the list as well
-
 const DLL = require('../lib/DLL');
 
 function deleteAllNodesWithValue(list, value) {
-  // your code here
+  // Start at the head of the list
+  let current = list.head;
+
+  // Traverse the entire list
+  while (current) {
+    // Save the next node now because current may be removed
+    const nextNode = current.next;
+
+    if (current.value === value) {
+      // If current is the head, move head forward
+      if (current === list.head) {
+        list.head = current.next;
+        if (list.head) {
+          list.head.prev = null;
+        } else {
+          // List became empty
+          list.tail = null;
+        }
+      } else if (current === list.tail) {
+        // If current is the tail, move tail backward
+        list.tail = current.prev;
+        if (list.tail) {
+          list.tail.next = null;
+        } else {
+          // List became empty
+          list.head = null;
+        }
+      } else {
+        // Middle node: stitch prev and next together
+        current.prev.next = current.next;
+        current.next.prev = current.prev;
+      }
+
+      // Decrement list size when a node is removed
+      if (typeof list.size === 'number') {
+        list.size--;
+      }
+    }
+
+    // Move to the saved next node
+    current = nextNode;
+  }
 }
 
+// Test
 const list = new DLL();
 list.insertAtBack(1);
 list.insertAtBack(2);
@@ -15,4 +54,4 @@ list.insertAtBack(2);
 list.insertAtBack(5);
 
 deleteAllNodesWithValue(list, 2);
-console.log(list.print()); // Output: 1 <-> 3 <-> 5
+console.log(list.print()); // Expected output: 1 <-> 3 <-> 5
